0aa107f6fc5d562c88b3421888e3315ef60424d1
tuuutuuu <rogertu@foxmail.com>
2016-03-23 19:59:55 +0800

    new web api for .NET client

---
+++ disconf-client/src/main/java/com/baidu/disconf/client/common/model/DisconfCenterFile.java
@@ -0,0 +1,214 @@
+package com.baidu.disconf.client.common.model;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.baidu.disconf.client.common.constants.SupportFileTypeEnum;
+import com.baidu.disconf.core.common.utils.ClassLoaderUtil;
+import com.baidu.disconf.core.common.utils.OsUtil;
+
+/**
+ * 配置文件表示
+ *
+ * @author liaoqiqi
+ * @version 2014-5-20
+ */
+public class DisconfCenterFile extends DisconfCenterBaseModel {
+
+    // -----key: 配置文件中的项名
+    // -----value: 默认值
+    private Map<String, FileItemValue> keyMaps = new HashMap<String, FileItemValue>();
+
+    // 额外的配置数据，非注解式使用它来存储
+    private Map<String, Object> additionalKeyMaps = new HashMap<String, Object>();
+
+    // 配置文件类
+    private Class<?> cls;
+
+    // 文件名
+    private String fileName;
+
+    private String copy2TargetDirPath;
+
+    // 文件类型
+    private SupportFileTypeEnum supportFileTypeEnum = SupportFileTypeEnum.ANY;
+
+    public Class<?> getCls() {
+        return cls;
+    }
+
+    public void setCls(Class<?> cls) {
+        this.cls = cls;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Map<String, FileItemValue> getKeyMaps() {
+        return keyMaps;
+    }
+
+    public void setKeyMaps(Map<String, FileItemValue> keyMaps) {
+        this.keyMaps = keyMaps;
+    }
+
+    public Map<String, Object> getAdditionalKeyMaps() {
+        return additionalKeyMaps;
+    }
+
+    public void setAdditionalKeyMaps(Map<String, Object> additionalKeyMaps) {
+        this.additionalKeyMaps = additionalKeyMaps;
+    }
+
+    public SupportFileTypeEnum getSupportFileTypeEnum() {
+        return supportFileTypeEnum;
+    }
+
+    public void setSupportFileTypeEnum(SupportFileTypeEnum supportFileTypeEnum) {
+        this.supportFileTypeEnum = supportFileTypeEnum;
+    }
+
+    public String getCopy2TargetDirPath() {
+        return copy2TargetDirPath;
+    }
+
+    public void setCopy2TargetDirPath(String copy2TargetDirPath) {
+        this.copy2TargetDirPath = copy2TargetDirPath;
+    }
+
+    @Override
+    public String toString() {
+        return "\n\tDisconfCenterFile [\n\tkeyMaps=" + keyMaps + "\n\tcls=" + cls + "\n\tfileName=" + fileName
+                + "\n\tcopy2TargetDirPath=" + copy2TargetDirPath +
+                super.toString() + "]";
+    }
+
+    @Override
+    public String infoString() {
+        return "\n\tDisconfCenterFile [\n\tkeyMaps=" + keyMaps + "\n" +
+                "\tadditionalKeyMaps=" + additionalKeyMaps + "\n\tcls=" + cls + super.infoString() + "]";
+    }
+
+    /**
+     * 获取可以表示的KeyMap对
+     */
+    public Map<String, Object> getKV() {
+
+        // 非注解式的
+        if (keyMaps.size() == 0) {
+            return additionalKeyMaps;
+        }
+
+        //
+        // 注解式的
+        //
+        Map<String, Object> map = new HashMap<String, Object>();
+        for (String key : keyMaps.keySet()) {
+            map.put(key, keyMaps.get(key).getValue());
+        }
+
+        return map;
+    }
+
+    /**
+     * 配置文件的路径
+     */
+    public String getFilePath() {
+
+        if (copy2TargetDirPath != null) {
+
+            if (copy2TargetDirPath.startsWith("/")) {
+                return OsUtil.pathJoin(copy2TargetDirPath, fileName);
+            }
+
+            return OsUtil.pathJoin(ClassLoaderUtil.getClassPath(), copy2TargetDirPath, fileName);
+        }
+
+        return OsUtil.pathJoin(ClassLoaderUtil.getClassPath(), fileName);
+    }
+
+    /**
+     * 配置文件的路径
+     */
+    public String getFileDir() {
+
+        // 获取相对于classpath的路径
+        if (copy2TargetDirPath != null) {
+
+            if (copy2TargetDirPath.startsWith("/")) {
+                return OsUtil.pathJoin(copy2TargetDirPath);
+            }
+
+            return OsUtil.pathJoin(ClassLoaderUtil.getClassPath(), copy2TargetDirPath);
+        }
+
+        return ClassLoaderUtil.getClassPath();
+    }
+
+    /**
+     * 配置文件Item项表示，包括了值，还有其类型
+     *
+     * @author liaoqiqi
+     * @version 2014-6-16
+     */
+    public static class FileItemValue {
+
+        private Object value;
+        private Field field;
+        private Method setMethod;
+
+        public Object getValue() {
+            return value;
+        }
+
+        public void setValue(Object value) {
+            this.value = value;
+        }
+
+        public Field getField() {
+            return field;
+        }
+
+        public void setField(Field field) {
+            this.field = field;
+        }
+
+        public Method getSetMethod() {
+            return setMethod;
+        }
+
+        public void setSetMethod(Method setMethod) {
+            this.setMethod = setMethod;
+        }
+
+        @Override
+        public String toString() {
+            return "FileItemValue{" +
+                    "value=" + value +
+                    ", field=" + field +
+                    ", setMethod=" + setMethod +
+                    '}';
+        }
+
+        public FileItemValue(Object value, Field field) {
+            super();
+            this.value = value;
+            this.field = field;
+
+        }
+
+        public FileItemValue(Object value, Field field, Method setMethod) {
+            super();
+            this.value = value;
+            this.field = field;
+            this.setMethod = setMethod;
+        }
+    }
+}
---
+++ disconf-web/src/main/java/com/baidu/disconf/web/service/config/dao/impl/ConfigDaoImpl.java
@@ -0,0 +1,159 @@
+package com.baidu.disconf.web.service.config.dao.impl;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Service;
+
+import com.baidu.disconf.core.common.constants.DisConfigTypeEnum;
+import com.baidu.disconf.web.common.Constants;
+import com.baidu.disconf.web.service.config.bo.Config;
+import com.baidu.disconf.web.service.config.dao.ConfigDao;
+import com.baidu.dsp.common.constant.DataFormatConstants;
+import com.baidu.dsp.common.dao.AbstractDao;
+import com.baidu.dsp.common.dao.Columns;
+import com.baidu.dsp.common.form.RequestListBase.Page;
+import com.baidu.dsp.common.utils.DaoUtils;
+import com.baidu.ub.common.db.DaoPage;
+import com.baidu.ub.common.db.DaoPageResult;
+import com.baidu.unbiz.common.genericdao.operator.Match;
+import com.baidu.unbiz.common.genericdao.operator.Modify;
+import com.baidu.unbiz.common.genericdao.operator.Order;
+import com.github.knightliao.apollo.utils.time.DateUtils;
+
+/**
+ * @author liaoqiqi
+ * @version 2014-6-16
+ */
+@Service
+public class ConfigDaoImpl extends AbstractDao<Long, Config> implements ConfigDao {
+    
+    private String SQL_SELECT_META = "select name, type, update_time from disconf.config where app_id=? and env_id=? and version=? and status=1";
+
+    /**
+     *
+     */
+    @Override
+    public Config getByParameter(Long appId, Long envId, String version, String key,
+                                 DisConfigTypeEnum disConfigTypeEnum) {
+
+        return findOne(new Match(Columns.APP_ID, appId), new Match(Columns.ENV_ID, envId),
+                new Match(Columns.VERSION, version), new Match(Columns.TYPE, disConfigTypeEnum.getType()),
+                new Match(Columns.NAME, key), new Match(Columns.STATUS, Constants.STATUS_NORMAL));
+    }
+
+    /**
+     *
+     */
+    @Override
+    public List<Config> getConfByAppEnv(Long appId, Long envId) {
+
+        if (envId == null) {
+            return find(new Match(Columns.APP_ID, appId), new Match(Columns.STATUS, Constants.STATUS_NORMAL));
+        } else {
+
+            return find(new Match(Columns.APP_ID, appId), new Match(Columns.ENV_ID, envId),
+                    new Match(Columns.STATUS, Constants.STATUS_NORMAL));
+
+        }
+    }
+
+    /**
+     *
+     */
+    @Override
+    public DaoPageResult<Config> getConfigList(Long appId, Long envId, String version, Page page) {
+
+        DaoPage daoPage = DaoUtils.daoPageAdapter(page);
+        List<Match> matchs = new ArrayList<Match>();
+
+        matchs.add(new Match(Columns.APP_ID, appId));
+
+        matchs.add(new Match(Columns.ENV_ID, envId));
+
+        matchs.add(new Match(Columns.VERSION, version));
+
+        matchs.add(new Match(Columns.STATUS, Constants.STATUS_NORMAL));
+
+        return page2(matchs, daoPage);
+    }
+
+    /**
+     *
+     */
+    @Override
+    public List<Config> getConfigList(Long appId, Long envId, String version) {
+
+        List<Match> matchs = new ArrayList<Match>();
+        matchs.add(new Match(Columns.APP_ID, appId));
+        matchs.add(new Match(Columns.ENV_ID, envId));
+        matchs.add(new Match(Columns.VERSION, version));
+        matchs.add(new Match(Columns.STATUS, Constants.STATUS_NORMAL));
+
+        return find(matchs, new ArrayList<Order>());
+    }
+
+    /**
+     * @param configId
+     */
+    @Override
+    public void deleteItem(Long configId) {
+        String curTime = DateUtils.format(new Date(), DataFormatConstants.COMMON_TIME_FORMAT);
+        List<Modify> modifyList = new ArrayList<Modify>();
+        modifyList.add(modify(Columns.STATUS, Constants.STATUS_DELETE));
+        modifyList.add(modify(Columns.UPDATE_TIME, curTime));
+
+        update(modifyList, match(Columns.CONFIG_ID, configId));
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void updateValue(Long configId, String value) {
+
+        // 时间
+        String curTime = DateUtils.format(new Date(), DataFormatConstants.COMMON_TIME_FORMAT);
+
+        List<Modify> modifyList = new ArrayList<Modify>();
+        modifyList.add(modify(Columns.VALUE, value));
+        modifyList.add(modify(Columns.UPDATE_TIME, curTime));
+
+        update(modifyList, match(Columns.CONFIG_ID, configId));
+    }
+
+    @Override
+    public String getValue(Long configId) {
+        Config config = get(configId);
+        return config.getValue();
+    }
+
+    @Override
+    public List<Config> getConfigMetas(Long appId, Long envId, String version) {
+        return this.findBySQL(SQL_SELECT_META, Arrays.asList(new Object[]{appId, envId, version}),
+                new RowMapper<Config>( ) {
+
+                    @Override
+                    public Config mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        Config config = new Config( );
+                        config.setName(rs.getString(Columns.NAME));
+                        config.setType(rs.getInt(Columns.TYPE));
+                        config.setUpdateTime(rs.getString(Columns.UPDATE_TIME));
+                        return config;
+                    }
+            
+        });
+    }
+
+    @Override
+    public List<Config> getConfigItems(Long appId, Long envId, String version) {
+        return find(new Match(Columns.APP_ID, appId), new Match(Columns.ENV_ID, envId),
+                new Match(Columns.VERSION, version), new Match(Columns.TYPE, DisConfigTypeEnum.ITEM.getType()),
+             new Match(Columns.STATUS, Constants.STATUS_NORMAL));
+    }  
+}
