144aa927012c8b85faa136068a030c7526814455
tuuutuuu <tuwenjie@ymt.corp>
2016-05-07 12:03:28 +0800

    fix NPE when Disconf beans not configured

--- disconf-client/src/main/java/com/baidu/disconf/client/DisConf.java
+++ disconf-client/src/main/java/com/baidu/disconf/client/DisConf.java
@@ -3,6 +3,7 @@ package com.baidu.disconf.client;
 import java.io.File;
 
 import com.baidu.disconf.client.common.model.DisconfCenterFile;
+import com.baidu.disconf.client.config.ConfigMgr;
 
 /**
  * 
@@ -11,13 +12,20 @@ import com.baidu.disconf.client.common.model.DisconfCenterFile;
  */
 public class DisConf {
 
-	
+
     /**
      * 如果需要，应用可以直接获取Disconf保存到本地的配置文件
+     * 
      * @param fileName
      * @return
      */
-    public static File getLocalConfig( String fileName ) {
+    public static File getLocalConfig(String fileName) {
+
+        if (!ConfigMgr.isInit()) {
+            throw new RuntimeException(
+                    "Disconf not init yet. Please make sure DisconfMgrBean & DisconfMgrBeanSecond are configured in Spring.");
+        }
         return new File(DisconfCenterFile.getFilePath(fileName));
+
     }
 }
